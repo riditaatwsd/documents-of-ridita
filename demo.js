5c183f08b58303f5c9c5ee236e9fa99f0fcdd339
riditaatwsd <fabiha.redita@wsd.com>
2026-08-06 13:14:18 +0600

    Update console log message in demo.js

--- demo.js
+++ demo.js
@@ -9,7 +9,7 @@ const targetNumber = Math.floor(Math.random() * 10) + 1;
 let attempts = 0;
 
 
-console.log('=== NUMBER GUESSING GAME === from test branch on august 6');
+console.log('=== NUMBER GUESSING GAME === from test branch on august 6...');
 console.log('I am thinking of a number between 1 and 10.');
 
 function askQuestion() {
